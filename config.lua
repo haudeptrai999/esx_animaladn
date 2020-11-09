9f859aa05534f74022fc5f07447f62e8c1d5bbe0
haudeptrai999 <67598326+haudeptrai999@users.noreply.github.com>
2020-11-09 18:45:42 +0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config                            = {}
+
+Config.Animaladn      = {
+    {label = 'Chó pug', value = 'animal_skin', maleModel = 'a_c_pug', femaleModel = 'a_c_pug'},
+    {label = 'Chó lông xù', value = 'animal_skin', maleModel = 'a_c_westy', femaleModel = 'a_c_westy'},
+	{label = 'Mèo', value = 'animal_skin', maleModel = 'a_c_cat_01', femaleModel = 'a_c_cat_01'},
+	{label = 'Bò', value = 'animal_skin', maleModel = 'a_c_cow', femaleModel = 'a_c_cow'},
+	{label = 'Thỏ', value = 'animal_skin', maleModel = 'a_c_rabbit_01', femaleModel = 'a_c_rabbit_01'},
+	{label = 'Gà', value = 'animal_skin', maleModel = 'a_c_hen', femaleModel = 'a_c_hen'},
+	{label = 'Sói', value = 'animal_skin', maleModel = 'a_c_coyote', femaleModel = 'a_c_coyote'},
+	{label = 'Cá mập sát thủ', value = 'animal_skin', maleModel = 'a_c_killerwhale', femaleModel = 'a_c_killerwhale'},
+	{label = 'Đại bàng', value = 'animal_skin', maleModel = 'a_c_chickenhawk', femaleModel = 'a_c_chickenhawk'},
+	{label = 'Chim hải âu', value = 'animal_skin', maleModel = 'a_c_seagull', femaleModel = 'a_c_seagull'},
+	{label = 'Vịt trời', value = 'animal_skin', maleModel = 'a_c_cormorant', femaleModel = 'a_c_cormorant'}
+}
