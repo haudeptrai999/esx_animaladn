e4c9267d2b6ac2d4328496890fb74bb3293cc69f
haudeptrai999 <67598326+haudeptrai999@users.noreply.github.com>
2020-11-09 19:27:46 +0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config                            = {}
+
+Config.Animaladn      = {
+    {label = 'Pug', value = 'animal_skin', maleModel = 'a_c_pug', femaleModel = 'a_c_pug'},
+    {label = 'Dog', value = 'animal_skin', maleModel = 'a_c_westy', femaleModel = 'a_c_westy'},
+	{label = 'Cat', value = 'animal_skin', maleModel = 'a_c_cat_01', femaleModel = 'a_c_cat_01'},
+	{label = 'Cow', value = 'animal_skin', maleModel = 'a_c_cow', femaleModel = 'a_c_cow'},
+	{label = 'Rabbit', value = 'animal_skin', maleModel = 'a_c_rabbit_01', femaleModel = 'a_c_rabbit_01'},
+	{label = 'Chicken', value = 'animal_skin', maleModel = 'a_c_hen', femaleModel = 'a_c_hen'},
+	{label = 'Wolf', value = 'animal_skin', maleModel = 'a_c_coyote', femaleModel = 'a_c_coyote'},
+	{label = 'Hunter whale', value = 'animal_skin', maleModel = 'a_c_killerwhale', femaleModel = 'a_c_killerwhale'},
+	{label = 'Eagle', value = 'animal_skin', maleModel = 'a_c_chickenhawk', femaleModel = 'a_c_chickenhawk'},
+	{label = 'Seagulls', value = 'animal_skin', maleModel = 'a_c_seagull', femaleModel = 'a_c_seagull'},
+	{label = 'Mallard', value = 'animal_skin', maleModel = 'a_c_cormorant', femaleModel = 'a_c_cormorant'}
+}
