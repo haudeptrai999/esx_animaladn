dd2413aee09a90d062320d3df3292cccc8e0e67d
haudeptrai999 <67598326+haudeptrai999@users.noreply.github.com>
2020-11-09 18:46:54 +0700

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,5 @@
+
+-- item
+
+INSERT INTO essentialmode.items (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('thuoc_doiadn', 'Change animal medicine', 5, 0, 1);
+INSERT INTO essentialmode.items (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('thuoc_giaiadn', 'Change to human medicine', 5, 0, 1);
